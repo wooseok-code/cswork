103f33ae2703707962b4e9bb49f248acbae39d00
wooseok <wooseokk89@gmail.com>
2023-12-19 16:36:19 +0900

    Distance of two points

--- Test01-Hello/Program.cs
+++ Test01-Hello/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using static System.Console;
+using static System.Math;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
@@ -10,10 +11,43 @@ using System.Threading.Tasks;
 
 namespace Test01_Hello
 {
+
+    static class myLib
+    {
+        public static string GetToken(string str, int n, char ch)        // str: "1 2 3" "1,2,3" n번째
+        {
+            return str.Split(ch)[n];
+        }
+    }
+
+    class Point
+    {
+        int x, y;
+
+        public Point(int x = 0, int y = 0)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public double Dist(Point p1, Point p2)
+        {
+            return Sqrt(Pow((p1.x - p2.x), 2) + Pow((p1.y - p2.y), 2));
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            Point point = new Point();
+
+            Point p1 = new Point(3, 4);
+            Point p2 = new Point(6, 9);
+            double dd = 0;
+            dd = point.Dist(p1, p2);
+            WriteLine($"distance : {dd:F2}\n"); // :F2 반올림하여 소수점 이하 두번째자리까지 출력
+
             /*          Program pgm = new Program();
                       int i = pgm.function();*/
             Test01 Sub = new Test01();
@@ -48,16 +82,17 @@ namespace Test01_Hello
             WriteLine($"i : {i}\nj : {j}\nd : {d}\ne : {e}\no : {o}");
 
             // ttt
-
+            // myLib my = new myLib(); // static으로 선언하면 쓰지않는다
             while (true)
             {
                 try
                 {
                     WriteLine("두 개의 정수를 입력하세요 : ");
                     string str = ReadLine();
-                    i = int.Parse(str.Split(' ')[0]);
+                    i = int.Parse(myLib.GetToken(str,0,' '));   // static일경우
+                    //i = int.Parse(my.GetToken(str,0,' '));
                     j = int.Parse(str.Split(' ')[1]);
-
+                    
                     WriteLine($"Hello World({i},{j})");   // printf 함수와 동일 brace안 숫자대로 들어감
 
 
